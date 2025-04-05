f2b825a8aed554e929e6e0db9e0a9936d3d05f24
Sid <sidtheminerofficial@gmail.com>
2025-04-05 15:07:18 +0530

    Modified Landing page and added user auth buttons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
+// app/layout.js
 import "./globals.css";
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
   title: "Documi",
@@ -8,43 +9,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html>
-        <body>
-          <div className="bg-[#181818] flex h-screen  w-full">
-            <SignedIn>
-              <div className="bg-[#f3f5ec] rounded-xl m-[1%] w-[80%]">
-                {children}
-              </div>
-            </SignedIn>
-            <SignedOut>
-              <div className="bg-[#181818] w-full h-screen flex flex-col items-center ">
-                <nav className=" px-[4%] py-[.5%]  shadow-xs ">
-                  
-                  <div className=" text-xl font-bold text-center  ">
-                    <h1 className=" ">Documi</h1>
-                  </div>
-
-
-                  <div className="flex gap-6 text-[#181818] font-medium">
-                    
-                  </div>
-                </nav>
-                <div className="bg-[#f3f5ec]  w-[98%] rounded-3xl h-screen flex flex-col justify-center items-center text-center  ">
-                
-                  <h1 className="font-bold text-7xl text-black ">
-                    Simplify the Paperwork.
-                  </h1>
-                  <h1 className="font-bold text-6xl text-[#C5C69A]  ">
-                    Power Your <span className="border-b-2 border-black"> Business</span>.
-                  </h1>
-                  <p className="text-gray-900 mt-8 text-sm italic font-bold ">Your one-stop tool to create, customize, and organize legal docs effortlessly</p>
-                  
-                </div>
-              </div>
-
-            </SignedOut>
-          </div>
-        </body>
+      <html lang="en">
+        <body className="bg-[#181818] h-screen w-full">{children}</body>
       </html>
     </ClerkProvider>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,43 @@
-function Home() {
+// app/page.js
+"use client";
+
+import { SignedIn, SignedOut, SignOutButton, SignUpButton, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function HomePage() {
   return (
-    <div className="bg-[#181818] flex h-screen p-[1%] w-full">
-      <div className="w-[20%] flex flex-col gap-4 p-4"></div>
-      <div className="bg-[#f3f5ec] rounded-xl w-[80%]"></div>
+    <div className="w-full h-screen flex flex-col items-center">
+      <nav className="px-[4%] py-[.5%] w-full flex justify-between items-center shadow-xs">
+        <h1 className="text-xl font-bold text-center text-white">Documi</h1>
+
+        <div className="flex flex-row gap-6 font-medium">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton />
+          </SignedOut>
+          <SignedIn>
+            <Link
+              href="/dashboard-home"
+              className="cursor-pointer px-8 border border-[#f3f5ec] p-2 rounded-3xl text-sm text-center font-light hover:bg-[#f3f5ec] hover:text-black transition duration-300 hover:scale-110"
+            >
+              Dashboard
+            </Link>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+
+      <div className="bg-[#f3f5ec] w-[98%] rounded-3xl h-full flex flex-col justify-center items-center text-center">
+        <h1 className="font-bold text-7xl text-black">
+          Simplify the Paperwork.
+        </h1>
+        <h1 className="font-bold text-6xl text-[#C5C69A]">
+          Power Your <span className="border-b-2 border-black"> Business</span>.
+        </h1>
+        <p className="text-gray-900 mt-8 text-sm italic font-bold">
+          Your one-stop tool to create, customize, and organize legal docs effortlessly
+        </p>
+      </div>
     </div>
   );
 }
-
-export default Home;
