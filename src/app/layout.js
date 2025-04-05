1dc3fe9711b8c6ead11f0aee7c7c98a9f81402b4
anshucodie <priyanshucodie@gmail.com>
2025-04-05 10:58:36 +0530

    adding basic dashboard

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,11 +18,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html>
+      <body>
+        <div className="bg-[#181818] flex h-screen p-[1%] w-full">
+          <div className="w-[20%] flex flex-col gap-4 p-4">
+            <h1 className="text-white text-2xl font-bold mb-4">Documi</h1>
+            <nav className="flex flex-col gap-2">
+              <a
+                href="/"
+                className="text-white hover:bg-[#2a2a2a] p-3 rounded-lg transition-colors"
+              >
+                Home
+              </a>
+              <a
+                href="/documents"
+                className="text-white hover:bg-[#2a2a2a] p-3 rounded-lg transition-colors"
+              >
+                Documents
+              </a>
+            </nav>
+          </div>
+          <div className="bg-[#f3f5ec] rounded-xl w-[80%]">{children}</div>
+        </div>
       </body>
     </html>
   );
