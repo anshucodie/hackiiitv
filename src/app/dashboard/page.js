7c349ef45adacaf131ee366423434c65afe8ee19
anshucodie <priyanshucodie@gmail.com>
2025-04-05 11:03:41 +0530

    updating dashboard

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Home</h1>
+      {/* Add your home content here */}
+    </div>
+  );
+}
+
+export default Home;
